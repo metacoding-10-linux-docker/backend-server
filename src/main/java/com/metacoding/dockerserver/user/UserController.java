87d0f832dd2401f974fc673a6e909ee280ff5c41
coderyu <153602328+coderyu5523@users.noreply.github.com>
2025-11-28 00:24:51 +0900

    UserController
    Removed index method and console log from findAll.

--- src/main/java/com/metacoding/dockerserver/user/UserController.java
+++ src/main/java/com/metacoding/dockerserver/user/UserController.java
@@ -9,16 +9,10 @@ import java.util.List;
 public class UserController {
 
     private final UserRepository userRepository;
-    
-    @GetMapping("/")
-    public String index() {
-        return "<h1>hello docker</h1>";
-    }
 
     @GetMapping("/api/users")
     public List<User> findAll() {
         List<User> users = userRepository.findAll();
-        System.out.println(users);
         return users;
     }
 }
