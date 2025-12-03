d725fafd39c1c068c39f0916dae07af867a088a9
coderyu5523 <coderyu5523@naver.com>
2025-12-03 22:53:50 +0900

    user

--- src/main/java/com/metacoding/dockerserver/user/UserController.java
+++ src/main/java/com/metacoding/dockerserver/user/UserController.java
@@ -10,14 +10,6 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-
-    @PostMapping("/login")
-    public User login(@RequestBody User user) {
-        User findUser = userRepository.findByUsername(user.getUsername()).orElseThrow(() -> new RuntimeException("회원정보가 없습니다."));
-        return findUser;
-    }
-
-
     @GetMapping("/api/users")
     public List<User> findAll() {
         List<User> users = userRepository.findAll();
